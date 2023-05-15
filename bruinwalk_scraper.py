08579050cb1c15450857f9636d0deaaf7dc7c9e1
Andrew Liu <33340487+Andrewl7127@users.noreply.github.com>
2023-05-15 16:42:52 -0700

    handling classes with no professors

--- bruinwalk_scraper.py
+++ bruinwalk_scraper.py
@@ -140,6 +140,7 @@ def get_professors(course_code):
         all_professors += professors
     
     return all_professors
+
 ###
 # Function to get all reviews for a course
 # @param course code
@@ -158,139 +159,167 @@ def scrape_reviews(course_code):
     # get professors
     professors = get_professors(course_code)
     
-    # iterate through all professors
-    for i in professors:
-        try:
-            # get professor name
-            start = '/professors/'
-            end = f'/{course_code}/'
-            start_index = i.index(start) + len(start)
-            end_index = i.index(end)
-            prof = i[start_index:end_index].replace('-', ' ').title()
+    if len(professors) == 0:
+        
+        # ping class page and get response, pass to soup 
+        url = f"{base_url}/classes/{course_code}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-            # ping professor page and get response, pass to soup 
-            url = f"{base_url}{i}"
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, "html.parser")
+        # extract course code
+        course_c = soup.find("span", class_="aggregate-type-badge").get_text()
 
-            # extract department name
-            dep = soup.find("div", class_="department-name").get_text().strip().replace('Department of ', '')
+        # extract course name
+        course_n = soup.find("div", class_="aggregate-header content-row").find('h2').get_text()
 
-            # extract course code
-            course_c = soup.find("span", class_="aggregate-type-badge").get_text()
+        # append to dataframe
+        df.loc[idx] = [course_c, course_n, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', math.nan, math.nan]
 
-            # extract course name
-            course_n = soup.find("div", class_="aggregate-header content-row").find('h2').get_text()
+    else:
+        # iterate through all professors
+        for i in professors:
+            try:
+                # get professor name
+                start = '/professors/'
+                end = f'/{course_code}/'
+                start_index = i.index(start) + len(start)
+                end_index = i.index(end)
+                prof = i[start_index:end_index].replace('-', ' ').title()
 
-            # extract overall score and users
-            overall_score = soup.find("div", class_="overall-score").get_text().replace(' ', '')
-            replacements = ['\n', '\t', ' ', 'OverallRating', 'Basedon', 'Users', 'User']
-            overall_users = soup.find("div", class_="overall-text").get_text()
-            for j in replacements:
-                overall_users = overall_users.replace(j, '')
-
-            # extract specific ratings
-            ratings = soup.find_all("div", class_="ind-rating")
-            options = ['Easiness', 'Clarity', 'Workload', 'Helpfulness']
-            course_ratings = {'Overall' : math.nan, 'Users': math.nan}
-            if overall_score != 'N/A':
-                course_ratings['Overall'] = float(overall_score)
-            if overall_users != '':
-                course_ratings['Users'] = float(overall_users)
-            for j in options:
-                course_ratings[j] = math.nan
-            replacements = [' 5 ', '\n', ' ', '\t', '/']
-            for j in ratings[:4]:
-                val = j.find("span", class_="value").get_text()
-                for k in replacements:
-                    val = val.replace(k, '')
-                for l in options:
-                    if l in j.get_text():
-                        if val != 'N/A':
-                            course_ratings[l] = float(val)
-            course_ratings = str(course_ratings)
-
-            # iterate through all pages
-            paginator = int(soup.find("div", class_="paginator").find_all("span")[1].get_text().replace('1 of ', ''))
-            for page in range(paginator):
-
-                # go to page and pass to soup
-                url = f"{base_url}{i}?page={page+1}"
+                # ping professor page and get response, pass to soup 
+                url = f"{base_url}{i}"
                 response = requests.get(url)
                 soup = BeautifulSoup(response.text, "html.parser")
 
-                # extract reviews
-                reviews = soup.find_all("div", class_="review reviewcard")
-                
-                # if no reviews
-                if len(reviews) == 0:
+                # extract department name
+                dep = soup.find("div", class_="department-name").get_text().strip().replace('Department of ', '')
+
+                # extract course code
+                course_c = soup.find("span", class_="aggregate-type-badge").get_text()
+
+                # extract course name
+                course_n = soup.find("div", class_="aggregate-header content-row").find('h2').get_text()
+
+                # extract overall score and users
+                overall_score = soup.find("div", class_="overall-score").get_text().replace(' ', '')
+                replacements = ['\n', '\t', ' ', 'OverallRating', 'Basedon', 'Users', 'User']
+                overall_users = soup.find("div", class_="overall-text").get_text()
+                for j in replacements:
+                    overall_users = overall_users.replace(j, '')
+
+                # extract specific ratings
+                ratings = soup.find_all("div", class_="ind-rating")
+                options = ['Easiness', 'Clarity', 'Workload', 'Helpfulness']
+                course_ratings = {'Overall' : math.nan, 'Users': math.nan}
+                if overall_score != 'N/A':
+                    course_ratings['Overall'] = float(overall_score)
+                if overall_users != '':
+                    course_ratings['Users'] = float(overall_users)
+                for j in options:
+                    course_ratings[j] = math.nan
+                replacements = [' 5 ', '\n', ' ', '\t', '/']
+                for j in ratings[:4]:
+                    val = j.find("span", class_="value").get_text()
+                    for k in replacements:
+                        val = val.replace(k, '')
+                    for l in options:
+                        if l in j.get_text():
+                            if val != 'N/A':
+                                course_ratings[l] = float(val)
+                course_ratings = str(course_ratings)
+
+                # iterate through all pages
+                paginator = int(soup.find("div", class_="paginator").find_all("span")[1].get_text().replace('1 of ', ''))
+                for page in range(paginator):
+
+                    # go to page and pass to soup
+                    url = f"{base_url}{i}?page={page+1}"
+                    response = requests.get(url)
+                    soup = BeautifulSoup(response.text, "html.parser")
+
+                    # extract reviews
+                    reviews = soup.find_all("div", class_="review reviewcard")
                     
-                    # append to dataframe and increment index
-                    df.loc[idx] = [course_c, course_n, dep, prof, course_ratings, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 0, 0]
-                    idx += 1
-                
-                else:
-                    # iterate through all reviews
-                    for j in reviews:
-
-                        # extract quarter and grade element
-                        quarter_and_grade = j.select('div[class^="row collapse"]')[0]
-                        quarter_year = quarter_and_grade.select('div')[0].get_text()
-                        grade = quarter_and_grade.select('div')[1].get_text()
-
-                        # extract quarter and year
-                        replacements = ['\n', ' ', 'Quarter:']
-                        for k in replacements:
-                            quarter_year = quarter_year.replace(k, '')
-                        if quarter_year == 'N/A':
-                            quarter = 'N/A'
-                            year = 'N/A'
-                        else:
-                            quarter_year = re.sub(r'([a-zA-Z])(\d)', r'\1 \2', quarter_year).split(' ')
-                            quarter = quarter_year[0]
-                            year = quarter_year[1]
-
-                        # extract grade
-                        replacements = ['\n', ' ', 'Grade:']
-                        for k in replacements:
-                            grade = grade.replace(k, '')
-
-                        # extract review date
-                        review_date = j.select('span[class^="date"]')[0].get_text()
-                        replacements = ['\n', ' ']
-                        for k in replacements:
-                            review_date = review_date.replace(k, '')
-                        if '.' in review_date:
-                            index = review_date.index('.')
-                            review_date = review_date[:index][:3] + review_date[index:]
-                        # convert to datetime, standardize formatting
-                        input_formats = ["%b.%d,%Y", "%B%d,%Y"]
-                        for k in input_formats:
-                            try:
-                                review_date = datetime.strptime(review_date, k)
-                                break
-                            except:
-                                pass
-                        output_format = "%m/%d/%Y"
-                        review_date = review_date.strftime(output_format)
-
-                        # extract review text
-                        review_text = j.find("div", class_="expand-area review-paragraph").get_text().replace('\n', '')
-
-                        # extract review upvote value
-                        review_upvote = int(j.find("span", class_="upvote-value").get_text())
-
-                        # extract review downvote value
-                        review_downvote = int(j.find("span", class_="downvote-value").get_text())
-
+                    # if no reviews
+                    if len(reviews) == 0:
+                        
                         # append to dataframe and increment index
-                        df.loc[idx] = [course_c, course_n, dep, prof, course_ratings, quarter, year, grade, review_date, review_text, review_upvote, review_downvote]
+                        df.loc[idx] = [course_c, course_n, dep, prof, course_ratings, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', math.nan, math.nan]
                         idx += 1
-        except:
-            pass
+                    
+                    else:
+                        # iterate through all reviews
+                        for j in reviews:
+
+                            # extract quarter and grade element
+                            quarter_and_grade = j.select('div[class^="row collapse"]')[0]
+                            quarter_year = quarter_and_grade.select('div')[0].get_text()
+                            grade = quarter_and_grade.select('div')[1].get_text()
+
+                            # extract quarter and year
+                            replacements = ['\n', ' ', 'Quarter:']
+                            for k in replacements:
+                                quarter_year = quarter_year.replace(k, '')
+                            if quarter_year == 'N/A':
+                                quarter = 'N/A'
+                                year = 'N/A'
+                            else:
+                                quarter_year = re.sub(r'([a-zA-Z])(\d)', r'\1 \2', quarter_year).split(' ')
+                                quarter = quarter_year[0]
+                                year = quarter_year[1]
+
+                            # extract grade
+                            replacements = ['\n', ' ', 'Grade:']
+                            for k in replacements:
+                                grade = grade.replace(k, '')
+
+                            # extract review date
+                            review_date = j.select('span[class^="date"]')[0].get_text()
+                            replacements = ['\n', ' ']
+                            for k in replacements:
+                                review_date = review_date.replace(k, '')
+                            if '.' in review_date:
+                                index = review_date.index('.')
+                                review_date = review_date[:index][:3] + review_date[index:]
+                            # convert to datetime, standardize formatting
+                            input_formats = ["%b.%d,%Y", "%B%d,%Y"]
+                            for k in input_formats:
+                                try:
+                                    review_date = datetime.strptime(review_date, k)
+                                    break
+                                except:
+                                    pass
+                            output_format = "%m/%d/%Y"
+                            review_date = review_date.strftime(output_format)
+
+                            # extract review text
+                            review_text = j.find("div", class_="expand-area review-paragraph").get_text().replace('\n', '')
+
+                            # extract review upvote value
+                            review_upvote = int(j.find("span", class_="upvote-value").get_text())
+
+                            # extract review downvote value
+                            review_downvote = int(j.find("span", class_="downvote-value").get_text())
+
+                            # append to dataframe and increment index
+                            df.loc[idx] = [course_c, course_n, dep, prof, course_ratings, quarter, year, grade, review_date, review_text, review_upvote, review_downvote]
+                            idx += 1
+            except:
+
+                # append to dataframe and increment index
+                df.loc[idx] = [course_c, course_n, dep, prof, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', math.nan, math.nan]
+                idx += 1
     
-    #drop duplicates
+    # replace N/A course ratings with N/A dictionary
+    options = ['Easiness', 'Clarity', 'Workload', 'Helpfulness']
+    course_ratings = {'Overall' : math.nan, 'Users': math.nan}
+    for i in options:
+        course_ratings[i] = math.nan
+    df['Course Ratings'] = df['Course Ratings'].replace('N/A', str(course_ratings))
+
+    # drop duplicates
     df = df.drop_duplicates(keep = 'first').reset_index(drop = True)
+
     return df
 
 ###
@@ -321,6 +350,7 @@ def scrape_courses(dept_code = None):
         df = pd.concat([df, scrape_reviews(courses[i])]).reset_index(drop = True)
         # save progress every 1000 in case need to restart
         if i % 1000 == 0:
+            print('UNIQUE:', len(df['Course Code'].unique()))
             df.to_csv('progress.csv', index = False)
         
     # drop duplicates
@@ -337,17 +367,22 @@ def sentiment_analysis(df):
     sentiment_analysis = pipeline("sentiment-analysis")
 
     # sentiment analysis on review text, label and score using hugging face
-    for i in tqdm(range(len(df))):
+    for i in range(len(df)):
 
         text = df.at[i, 'Review Text']
-        # if greater than character limit truncate down to max
-        if len(text) > 512:
-            text = text[:512]
+        if text != 'N/A':
+            # if greater than character limit truncate down to max
+            if len(text) > 512:
+                text = text[:512]
 
-        # perform sentiment analysis
-        sentiment_results = sentiment_analysis(text)
-        df.at[i, 'Review Sentiment Label'] = sentiment_results[0]["label"]
-        df.at[i, 'Review Sentiment Score'] = sentiment_results[0]["score"]
+            # perform sentiment analysis
+            sentiment_results = sentiment_analysis(text)
+            df.at[i, 'Review Sentiment Label'] = sentiment_results[0]["label"]
+            df.at[i, 'Review Sentiment Score'] = sentiment_results[0]["score"]
+
+        else:
+            df.at[i, 'Review Sentiment Label'] = math.nan
+            df.at[i, 'Review Sentiment Score'] = math.nan
         
     return df
 
