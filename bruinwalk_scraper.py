96b6f7e6a7139d6b2da6c9965c86adb99eafdb36
Andrew Liu <33340487+Andrewl7127@users.noreply.github.com>
2023-05-15 14:12:41 -0700

    minor fixes

--- bruinwalk_scraper.py
+++ bruinwalk_scraper.py
@@ -140,7 +140,6 @@ def get_professors(course_code):
         all_professors += professors
     
     return all_professors
-
 ###
 # Function to get all reviews for a course
 # @param course code
@@ -229,63 +228,64 @@ def scrape_reviews(course_code):
                     # append to dataframe and increment index
                     df.loc[idx] = [course_c, course_n, dep, prof, course_ratings, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 0, 0]
                     idx += 1
-                    
-                # iterate through all reviews
-                for j in reviews:
+                
+                else:
+                    # iterate through all reviews
+                    for j in reviews:
 
-                    # extract quarter and grade element
-                    quarter_and_grade = j.select('div[class^="row collapse"]')[0]
-                    quarter_year = quarter_and_grade.select('div')[0].get_text()
-                    grade = quarter_and_grade.select('div')[1].get_text()
+                        # extract quarter and grade element
+                        quarter_and_grade = j.select('div[class^="row collapse"]')[0]
+                        quarter_year = quarter_and_grade.select('div')[0].get_text()
+                        grade = quarter_and_grade.select('div')[1].get_text()
 
-                    # extract quarter and year
-                    replacements = ['\n', ' ', 'Quarter:']
-                    for k in replacements:
-                        quarter_year = quarter_year.replace(k, '')
-                    if quarter_year == 'N/A':
-                        quarter = 'N/A'
-                        year = 'N/A'
-                    else:
-                        quarter_year = re.sub(r'([a-zA-Z])(\d)', r'\1 \2', quarter_year).split(' ')
-                        quarter = quarter_year[0]
-                        year = quarter_year[1]
+                        # extract quarter and year
+                        replacements = ['\n', ' ', 'Quarter:']
+                        for k in replacements:
+                            quarter_year = quarter_year.replace(k, '')
+                        if quarter_year == 'N/A':
+                            quarter = 'N/A'
+                            year = 'N/A'
+                        else:
+                            quarter_year = re.sub(r'([a-zA-Z])(\d)', r'\1 \2', quarter_year).split(' ')
+                            quarter = quarter_year[0]
+                            year = quarter_year[1]
 
-                    # extract grade
-                    replacements = ['\n', ' ', 'Grade:']
-                    for k in replacements:
-                        grade = grade.replace(k, '')
+                        # extract grade
+                        replacements = ['\n', ' ', 'Grade:']
+                        for k in replacements:
+                            grade = grade.replace(k, '')
 
-                    # extract review date
-                    review_date = j.select('span[class^="date"]')[0].get_text()
-                    replacements = ['\n', ' ']
-                    for k in replacements:
-                        review_date = review_date.replace(k, '')
-                    if '.' in review_date:
-                        index = review_date.index('.')
-                        review_date = review_date[:index][:3] + review_date[index:]
-                    # convert to datetime, standardize formatting
-                    input_formats = ["%b.%d,%Y", "%B%d,%Y"]
-                    for k in input_formats:
-                        try:
-                            review_date = datetime.strptime(review_date, k)
-                            break
-                        except:
-                            pass
-                    output_format = "%m/%d/%Y"
-                    review_date = review_date.strftime(output_format)
+                        # extract review date
+                        review_date = j.select('span[class^="date"]')[0].get_text()
+                        replacements = ['\n', ' ']
+                        for k in replacements:
+                            review_date = review_date.replace(k, '')
+                        if '.' in review_date:
+                            index = review_date.index('.')
+                            review_date = review_date[:index][:3] + review_date[index:]
+                        # convert to datetime, standardize formatting
+                        input_formats = ["%b.%d,%Y", "%B%d,%Y"]
+                        for k in input_formats:
+                            try:
+                                review_date = datetime.strptime(review_date, k)
+                                break
+                            except:
+                                pass
+                        output_format = "%m/%d/%Y"
+                        review_date = review_date.strftime(output_format)
 
-                    # extract review text
-                    review_text = j.find("div", class_="expand-area review-paragraph").get_text().replace('\n', '')
+                        # extract review text
+                        review_text = j.find("div", class_="expand-area review-paragraph").get_text().replace('\n', '')
 
-                    # extract review upvote value
-                    review_upvote = int(j.find("span", class_="upvote-value").get_text())
+                        # extract review upvote value
+                        review_upvote = int(j.find("span", class_="upvote-value").get_text())
 
-                    # extract review downvote value
-                    review_downvote = int(j.find("span", class_="downvote-value").get_text())
+                        # extract review downvote value
+                        review_downvote = int(j.find("span", class_="downvote-value").get_text())
 
-                    # append to dataframe and increment index
-                    df.loc[idx] = [course_c, course_n, dep, prof, course_ratings, quarter, year, grade, review_date, review_text, review_upvote, review_downvote]
-                    idx += 1
+                        # append to dataframe and increment index
+                        df.loc[idx] = [course_c, course_n, dep, prof, course_ratings, quarter, year, grade, review_date, review_text, review_upvote, review_downvote]
+                        idx += 1
         except:
             pass
     
@@ -300,19 +300,24 @@ def scrape_reviews(course_code):
 ###
 def scrape_courses(dept_code = None):
     
-    try:
-        # save progress
-        df = pd.read_csv('progress.csv')
-    except:
+    if dept_code == None:
+        try:
+            # save progress
+            df = pd.read_csv('progress.csv')
+        except:
+            # create dataframe
+            col_names = ['Course Code', 'Course Name', 'Department', 'Professor', 'Course Ratings', 'Quarter', 'Year', 'Grade', 'Review Date', 'Review Text', 'Review Upvote', 'Review Downvote']
+            df = pd.DataFrame(columns = col_names)
+    else:
         # create dataframe
         col_names = ['Course Code', 'Course Name', 'Department', 'Professor', 'Course Ratings', 'Quarter', 'Year', 'Grade', 'Review Date', 'Review Text', 'Review Upvote', 'Review Downvote']
         df = pd.DataFrame(columns = col_names)
-    
+        
     # get courses
     courses = get_courses(dept_code)
     
     # iterate through all courses and scrape reviews
-    for i in tqdm(range(len(df['Course Code'].unique()), len(courses))):
+    for i in tqdm(range(len(df), len(courses))):
         df = pd.concat([df, scrape_reviews(courses[i])]).reset_index(drop = True)
         # save progress every 1000 in case need to restart
         if i % 1000 == 0:
